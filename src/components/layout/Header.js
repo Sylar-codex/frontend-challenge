7646f4104e62276064c816185e6caadee50af55c
sylar-codex <vicaremy@gmail.com>
2024-01-27 16:10:31 +0100

    added some functionalities

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import search from "../../assets/icons/Icon - Search.svg";
 import calendar from "../../assets/icons/solar_calendar-linear.svg";
 import bell from "../../assets/icons/solar_bell-outline.svg";
@@ -8,9 +8,12 @@ import calendarDark from "../../assets/icons/dark-mode/solar_calendar-linear_dar
 import bellDark from "../../assets/icons/dark-mode/solar_bell-outline_dark.svg";
 import arrowDownDark from "../../assets/icons/dark-mode/Arrow - Down 2_dark.svg";
 import logo from "../../assets/icons/Logo.svg";
+import chevronRight from "../../assets/icons/chevron-right-70.svg";
 
 function Header({ darkMode }) {
   const today = new Date();
+
+  const [dropDown, setDropDown] = useState(false);
 
   const options = {
     day: "numeric",
@@ -55,27 +58,37 @@ function Header({ darkMode }) {
             {today.toLocaleDateString("en-GB", options)}
           </p>
         </div>
-        {/* profile info div */}
 
         <div className="flex lg:flex-row flex-row-reverse lg:justify-normal justify-between items-center lg:space-x-5 mt-2 lg:mt-0">
           <div className="lg:w-8 lg:h-8 w-9 h-9 rounded-full border-headerStroke border-2 border-solid p-1 flex justify-center items-center">
             <img className="w-full" src={darkMode ? bellDark : bell} alt="" />
           </div>
-          <div className="flex p-2 lg:w-64 w-20 h-14 rounded-full space-x-2 lg:space-x-0 lg:border-2 lg:border-solid lg:border-headerStroke lg:justify-between justify-center items-center">
-            <div className="rounded-full w-12 h-12">
-              <img className="w-full h-full" src={headerProfilePic} alt="" />
+          {/* profile info div */}
+          <div className="relative lg:w-64 w-20">
+            <div
+              onClick={() => {
+                setDropDown((prev) => !prev);
+              }}
+              className="flex hover:cursor-pointer p-2 w-full h-14 rounded-full space-x-2 lg:space-x-0 lg:border-2 lg:border-solid lg:border-headerStroke lg:justify-between justify-center items-center"
+            >
+              <div className="rounded-full w-12 h-12">
+                <img className="w-full h-full" src={headerProfilePic} alt="" />
+              </div>
+              <div className="hidden lg:block">
+                <h2>Justin Bergson</h2>
+                <p className="text-sm text-[#787486]">Justin@gmail.com</p>
+              </div>
+              <div>
+                <img
+                  className="w-5 h-5 hover:cursor-pointer"
+                  src={darkMode ? arrowDownDark : arrrowDown}
+                  alt=""
+                />
+              </div>
             </div>
-            <div className="hidden lg:block">
-              <h2>Justin Bergson</h2>
-              <p className="text-sm text-[#787486]">Justin@gmail.com</p>
-            </div>
-            <div>
-              <img
-                className="w-5 h-5 hover:cursor-pointer"
-                src={darkMode ? arrowDownDark : arrrowDown}
-                alt=""
-              />
-            </div>
+            {dropDown && (
+              <GoToProfile darkMode={darkMode} setDropDown={setDropDown} />
+            )}
           </div>
         </div>
       </div>
@@ -84,3 +97,30 @@ function Header({ darkMode }) {
 }
 
 export default Header;
+
+function GoToProfile({ darkMode, setDropDown }) {
+  return (
+    <div
+      onClick={() => {
+        setDropDown(false);
+      }}
+      className={`absolute top-16 hover:cursor-pointer left-0 rounded-xl border-headerStroke lg:text-base text-sm shadow-lg w-64 ${
+        darkMode ? "bg-lighter-grey" : "bg-white"
+      } z-10`}
+    >
+      <div className="flex items-center justify-between p-2">
+        <div className="rounded-full w-12 h-12">
+          <img className="w-full h-full" src={headerProfilePic} />
+        </div>
+        <div>
+          <h3>Justin Bergson</h3>
+          <p className="text-sm text-[#787486]">Justin@gmail.com</p>
+          <p className="text-primary text-sm">View Profile</p>
+        </div>
+        <div className="w-5 h-5">
+          <img className="w-full h-full" src={chevronRight} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
